311d492784420d1c907a4780e5169ceaf2bf84f5
Madhavee Liyanagei <liyanageimadhavee@gmail.com>
2021-08-22 18:46:02 +0530

    Additional lab sheet

---
+++ app/src/main/java/com/example/additionallabsheet/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.additionallabsheet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnStart;
+    private Button btnStop;
+
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = (Button) findViewById(R.id.start);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+        btnStop = (Button) findViewById(R.id.stop);
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+    }
+
+}
